7695e0c5f58738cf2671c3c7e3da0b6d5f09ca08
Chau Que Nhon <82493653+cqnsinger@users.noreply.github.com>
2021-11-22 16:41:15 +0700

    Add files via upload

---
+++ graph.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+
+//resize console
+void resizeConsole(int width, int height)
+{
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+	MoveWindow(console, r.left, r.top, width, height, TRUE);
+}
+//Hide 
+void Nocurs()
+{
+	CONSOLE_CURSOR_INFO Info;
+	Info.bVisible = FALSE;
+	Info.dwSize = 20;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+}
+// change text color
+void textcolor(int x)
+{
+	HANDLE mau;
+	mau=GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(mau,x);
+}
+// move point
+void gotoxy(int x,int y)
+{    
+	HANDLE hConsoleOutput;    
+	COORD Cursor_an_Pos = {x-1,y-1};   
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);    
+	SetConsoleCursorPosition(hConsoleOutput , Cursor_an_Pos);
+}
+// Clear screen
+void ClrSrc()
+{
+	HANDLE hOut;
+	COORD Position;
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	Position.X = 0;
+	Position.Y = 0;
+	SetConsoleCursorPosition(hOut, Position);
+}
+
